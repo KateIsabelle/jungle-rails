1a5cd6a6fc8c6e4ff601ceb505d46e51d17e9d6b
KateIsabelle <kate.davis887@gmail.com>
2021-05-03 23:00:06 +0000

    update User password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
-  validates :password_digest, presence: true
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
 
   
 end
