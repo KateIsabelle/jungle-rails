0c0761440349a474fc5919862d1fce091a6769dd
KateIsabelle <kate.davis887@gmail.com>
2021-05-03 21:02:10 +0000

    add validation into User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
 
   has_secure_password
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+
   
 end
