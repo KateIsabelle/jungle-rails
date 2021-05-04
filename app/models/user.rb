feeb37b7a711e17d328960070b4f317633e8ea6a
KateIsabelle <kate.davis887@gmail.com>
2021-05-04 00:00:03 +0000

    start building first user spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,4 @@ class User < ActiveRecord::Base
   validates :password, confirmation: true
   validates :password_confirmation, presence: true
 
-  
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+
+    context "given first name, last name, password, and password confirmation" do
+      it "saves successfully" do
+        @user = User.create(first_name, last_name, password: "password", password_confirmation)
+        expect(Product.count).to eq(1)
+        expect(@product.errors.full_messages.length).to eq(0)
+      end
+    end
+
+  end
 end
