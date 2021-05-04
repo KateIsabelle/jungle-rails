71987ce2eaf6d3b14361eae76789afe7d4ff9931
KateIsabelle <kate.davis887@gmail.com>
2021-05-04 19:09:52 +0000

    check for products in home page spec

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -5,9 +5,10 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
   scenario "They see all products" do
     # ACT
     visit root_path
-
     # DEBUG / VERIFY
     save_screenshot
+    # we expect the user to see on the page
+    expect(page).to have_css 'article.product'
   end
 
 end
